30b82c478a1f54fce2f036de2d85f73dc43cb8d8
Stefany Hikaru Iwashita <20260266-ieg@instituto.ad>
2026-06-25 19:25:54 -0300

    capa do trabalho, voces podem alterar

---
+++ capa.js
@@ -0,0 +1,11 @@
+const direita = document.getElementById("direita");
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowRight") {
+        window.location.href = "index.html";
+    }
+});
+
+direita.addEventListener("click", () => {
+        window.location.href = "index.html";
+});
