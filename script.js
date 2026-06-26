1452d944680b872c73731ac716eea6cec0329e74
Stefany Hikaru Iwashita <20260266-ieg@instituto.ad>
2026-06-26 12:34:00 -0300

    abababbab

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const direita = document.getElementById("direita");
 
 document.addEventListener("keydown", (event) => {
     if (event.key === "ArrowRight") {
-        window.location.href = "index.html";
+        window.location.href = "pag1.html";
     }
 });
 
