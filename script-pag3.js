9f939de5b71f80e097a401777d239c50632223cc
Stefany Hikaru Iwashita <20260266-ieg@instituto.ad>
2026-06-28 12:10:22 -0300

    fiz uma parte do slide 3 e botei animação na capa

--- script-pag3.js
+++ script-pag3.js
@@ -1,5 +1,9 @@
 const direita = document.getElementById("direita");
 const esquerda = document.getElementById("esquerda");
+const qtdSalas = document.getElementById("qtd-salas");
+const qtdQuadros = document.getElementById("qtd-quadros");
+const qtdEsculturas = document.getElementById("qtd-esculturas");
+const qtdFotos = document.getElementById("qtd-fotos");
 
 document.addEventListener("keydown", (event) => {
     if (event.key === "ArrowRight") {
@@ -18,3 +22,23 @@ direita.addEventListener("click", () => {
 esquerda.addEventListener("click", () => {
     window.location.href = "pag2.html";
 });
+
+function count(elemento, qtd, interval, timeout) {
+    setTimeout(() => {
+        let cont = 1;
+        const timer = setInterval(() => {
+            elemento.textContent = cont;
+
+            cont++
+
+            if (cont === qtd + 1) {
+                clearInterval(timer)
+            }
+        }, interval);
+    }, timeout);
+}
+
+count(qtdSalas, 15, 20, 550);
+count(qtdQuadros, 180, 2, 1200);
+count(qtdEsculturas, 50, 10, 2400);
+count(qtdFotos, 80, 5, 3150);
